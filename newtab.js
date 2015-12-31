fb1844e1358a2e57051edc145993bd9f728ffd8a
jinalex <alex.b.jin@gmail.com>
2015-12-30 19:53:06 -0500

    v2

--- newtab.js
+++ newtab.js
@@ -1,58 +1,95 @@
-  function initClock() {
+function hyperlink(elem) {
+    window.location.href = elem.target.getAttribute('href');
+}
+
+function add_bookmark(image_num, url) {
+    var bookmark_card = document.createElement('div');
+    bookmark_card.setAttribute('class','bookmark pad-top');
+
+    var link_img = document.createElement('img');
+    link_img.setAttribute('name', image_num);
+    link_img.src = './images/icons/'+image_num+'.png';
+    link_img.addEventListener('click', hyperlink);
+    link_img.setAttribute('href', url);
+    link_img.setAttribute('title', url);
+
+    bookmark_card.appendChild(link_img);
+
+    document.getElementById('bookmark_bar').appendChild(bookmark_card);
+}
+
+function initClock() {
 
     //defaults
     chrome.storage.sync.get({
       'interval': 'years',
-      'startDate': '1996-11-01'
+      'startDate': '2000-01-01',
+      'startTime': '',
+      'bookmark_list': {},
+      'last_UID': 101,
+      'count_what': ''
     }, function(items) {
 
-        var startTime = items.startDate;
+        var startDate = items.startDate;
         var selectedUnits = items.interval;
+        var count_what = items.count_what;
+        var before_after;
 
-    var _MS_PER_SEC = 1000;
-    var _MS_PER_DAY = 1000 * 60 * 60 * 24;
-    var _MS_PER_YEAR = 1000 * 3600 * 24 *365;
-    
-    // var birthTime = 'November 1, 1996 00:00:00';
-    var start = new Date(startTime);
-    
-    var conversion;
-    var decimals;
+        var _MS_PER_SEC = 1000;
+        var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+        var _MS_PER_YEAR = 1000 * 3600 * 24 *365;
+        
+        // var birthTime = 'November 1, 1996 00:00:00';
+        
+        var conversion;
+        var decimals;
 
-    // seconds
-    if (selectedUnits == 'seconds') {
-        conversion = _MS_PER_SEC;    
-        decimals = 1;
-    }
+        // seconds
+        if (selectedUnits == 'seconds') {
+            conversion = _MS_PER_SEC;    
+            decimals = 1;
+        }
 
-    // days
-    else if (selectedUnits == 'days') {
-        conversion = _MS_PER_DAY;
-        decimals = 7;
-    }
+        // days
+        else if (selectedUnits == 'days') {
+            conversion = _MS_PER_DAY;
+            decimals = 7;
+        }
 
-    // years
-    else if (selectedUnits == 'years') {
-        conversion = _MS_PER_YEAR;
-        decimals = 9;
-    }
+        // years
+        else if (selectedUnits == 'years') {
+            conversion = _MS_PER_YEAR;
+            decimals = 9;
+        }
 
-    var settings = {
-        'startTime': start,
-        'conversion': conversion,
-        'decimals': decimals
-    }
+        var now = new Date()
+        if (startDate.getTime() > now.getTime()){
+            before_after = "until ";
+        } else {
+            before_after = "since ";
+        }
 
-    updateClock(start, conversion, decimals, selectedUnits);
+        if (count_what.length > 0){
+            count_what = before_after+count_what;
+        }
 
+        updateClock(startDate, conversion, decimals, selectedUnits, count_what);
+
+        // populates bookmarks
+        for (var key in items.bookmark_list) {
+            if (items.bookmark_list.hasOwnProperty(key)) {
+                add_bookmark(items.bookmark_list[key].img, items.bookmark_list[key].url_link, key);
+            }
+        }
     });
   }
 
-  function updateClock(startTime, conversion, decimals, message) {
+  function updateClock(startTime, conversion, decimals, message, count_what) {
     setInterval(function(){
         var now = new Date() // current date
         var timeDiff = Math.abs(now.getTime() - startTime.getTime());
-        document.getElementById('time').innerHTML = (timeDiff/conversion).toFixed(decimals) + "<small> "+message+"</small>";
+        document.getElementById('time').innerHTML = (timeDiff/conversion).toFixed(decimals);
+        document.getElementById('what').innerHTML = "<small> "+message+"</small>"+"<p> "+count_what+"</p>";
     }, 100);
   }
 
--- options.js
+++ options.js
@@ -1,22 +1,126 @@
+// Bookmarks------------
+var num_icons = 53;
+var selected_img = '01';
+var last_UID;
 
-// Options
+// Bookmark Constructor
+// function new_bookmark(imgNum, link) {
+    
+//     link_url = (typeof link_url === 'undefined') ? "#" : link_url;
+    
+//     this.imgNum = imgNum;
+//     this.link_url = link_url;
+// }
+
+function save_reminder() {
+    document.getElementById("reminder").setAttribute('class', 'centered');
+}
+
+function delete_bookmark(bkmk) {
+    bkmk.target.parentNode.remove();
+}
+
+function add_bookmark(image_num, url, UID) {
+    image_num = typeof image_num !== 'object' ?  image_num : selected_img;
+    UID = typeof UID !== 'undefined' ?  UID : last_UID+1;
+    last_UID = UID + 1;
+
+    if (document.getElementById('url_textfield').value.length > 0){
+       url = document.getElementById('url_textfield').value;
+    } else if (typeof url == 'undefined'){
+      url = '/options.html';
+    }
+
+    if (url.indexOf('://') == -1 && url.indexOf('/options.html') == -1) {
+      url = 'https://'+url
+    }
+
+    var bookmark_card = document.createElement('div');
+    bookmark_card.setAttribute('class','bookmark pad-top');
+    bookmark_card.setAttribute('id', UID);
+
+    var link_img = document.createElement('img');
+    link_img.setAttribute('name', image_num);
+    link_img.src = './images/icons/'+image_num+'.png';
+    link_img.setAttribute('target', '_blank');
+
+    var link_url = document.createElement('a');
+    link_url.setAttribute('href', url);
+    link_url.setAttribute('target', '_blank');
+    if (url != '/options.html'){
+      link_url.innerHTML = url.slice(url.indexOf('://')+3);
+    } else {
+      link_url.innerHTML = url;
+    }
+
+    var delete_btn = document.createElement('input');
+    delete_btn.setAttribute('class', 'btn btn-alert btn-sm right');
+    delete_btn.type = 'button';
+    delete_btn.value = 'X';
+
+    bookmark_card.appendChild(link_img);
+    bookmark_card.appendChild(link_url);
+    bookmark_card.appendChild(delete_btn);
+    delete_btn.addEventListener('click', delete_bookmark);
+    delete_btn.addEventListener('click', save_reminder);
+
+    document.getElementById('bookmark_container').appendChild(bookmark_card);
+    document.getElementById('url_textfield').value = "";
+}
+
+function select_icon(img) {
+    var target_id = img.target.id;
+    document.getElementById("icon"+selected_img).className = "icon";
+    document.getElementById(target_id).className = "icon highlight";
+    selected_img = target_id.slice(-2);
+}
+
+//---------------------
 
 // Saves options to chrome.storage
 function save_options() {
   
-  var date = document.getElementById('startDate').value;
-
+  var date = new Date(document.getElementById('startDate').value);
   var timeUnits = document.getElementsByName('interval');
+  var count_what = document.getElementById("count_what").value;
 
+  var bookmarks = document.getElementById("bookmark_container").children
+  var bookmark_list = {};
+
+  // save the links on screen
+  for (var i = 0; i < bookmarks.length; i++) {
+    last_UID = Math.max(last_UID, parseInt(bookmarks[i].getAttribute('id')));
+    bookmark_list[bookmarks[i].getAttribute('id')] = {
+      'img':bookmarks[i].children[0].getAttribute('name'),
+      'url_link':bookmarks[i].children[1].getAttribute('href')
+    };
+  };
+
+  console.log("list after save");
+  console.log(bookmark_list);
+
+  console.log("date after save");
+  console.log(date);
+
+  console.log("count_what");
+  console.log(count_what);
+
+  // Finds whether seconds, days or years is checked
   for(var i = 0; i < timeUnits.length; i++) {
      if(timeUnits[i].checked == true) {
          var selectedUnit = timeUnits[i].value;
      }
-   }
+  }
+
+  document.getElementById("reminder").setAttribute('class', 'centered hidden');
 
   chrome.storage.sync.set({
     'interval': selectedUnit,
-    'startDate': date
+    'startDate': date,
+    'bookmark_list': bookmark_list,
+    'last_UID': last_UID,
+    'count_what': count_what
+
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('save');
@@ -34,9 +138,13 @@ function restore_options() {
   // Use default value 
   chrome.storage.sync.get({
     'interval': 'years',
-    'startDate': '1996-11-01'
+    'startDate': '2000-01-01',
+    'bookmark_list': {},
+    'last_UID': 101,
+    'count_what': ''
   }, function(items) {
 
+    // checks off the selected time units
     var timeUnits = document.getElementsByName('interval');
     for(var i = 0; i < timeUnits.length; i++) {
        if(timeUnits[i].value == items.interval) {
@@ -44,12 +152,60 @@ function restore_options() {
        }
      }
 
+    // populates bookmarks
+    for (var key in items.bookmark_list) {
+        if (items.bookmark_list.hasOwnProperty(key)) {
+            add_bookmark(items.bookmark_list[key].img, items.bookmark_list[key].url_link, key);
+        }
+    }
+
+    document.getElementById('count_what').value = items.count_what;
+
+    var now = new Date();
+    var target_time = new Date(items.startDate);
+
     document.getElementById('startDate').value = items.startDate;
+    if(target_time.getTime() > now.getTime() && document.getElementById('count_what').value == '') {
+      document.getElementById('count_what').setAttribute('placeholder', 'Counting down to what?');
+    } else if (document.getElementById('count_what').value == '') {
+      document.getElementById('count_what').setAttribute('placeholder', 'Counting up from what?');
+    }
+
+    var bookmark_list = items.bookmark_list;
+
+
+    // Get last_UID
+    last_UID = items.last_UID;
+
+    console.log(bookmark_list);
   });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('add_bookmark').addEventListener('click', add_bookmark);
+document.getElementById('add_bookmark').addEventListener('click', save_reminder);
+
+window.onload = function() {
+  for (var i = 1; i < num_icons+1; i++){
+    if (i < 10) {
+      i = '0'+i
+    }
+    var option_icon = document.createElement('img');
+    option_icon.setAttribute('class', 'icon');
+    option_icon.src = './images/icons/'+i+'.png';
+    option_icon.setAttribute('id', 'icon'+i);
+    document.getElementById('icon_select').appendChild(option_icon);
+  }
+  for (var i = 1; i < num_icons+1; i++) {
+    if (i < 10) {
+      i = '0'+i
+    }
+    document.getElementById('icon'+i).addEventListener("click", select_icon);  
+  }
+  document.getElementById("icon"+selected_img).className = "icon highlight";
+}
+
 
 chrome.storage.sync.get(null, function(items) {
     try {
